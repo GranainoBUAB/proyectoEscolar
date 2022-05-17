d094aa0363fec43753a9913be98c61c802878ffe
GranainoBUAB <josemiguel@quesada.eu>
2022-05-17 18:44:18 +0200

    add pivot table course-user

---
+++ database/migrations/2022_05_17_164019_create_course_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user');
+            $table->bigInteger('id_course');
+            $table->integer('notas');
+            $table->integer('trimestre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_user');
+    }
+};
